d817d519296dedbeb0a3a4659a6acd2287291e2b
unknown <nhannguyenthien01234@gmail.com>
2022-03-19 16:37:15 +0700

    version0

---
+++ QLSV/js/index.js
@@ -0,0 +1,58 @@
+document.querySelector('#btnHienThi').onclick = function(){
+  // Lấy thông tin ng dùng nhập từ giao diện
+    var maSinhVien = document.querySelector('#txtMaSV').value;
+    var tenSinhVien = document.querySelector('#txtTenSV').value;
+    var loaiSinhVien = document.querySelector('#loaiSV').value;
+    var diemToan = document.querySelector('#txtDiemToan').value ;
+    var diemVan = document.querySelector('#txtDiemVan').value ;
+
+
+console.log('maSinhVien',maSinhVien);
+console.log('tenSinhVien',tenSinhVien);
+console.log('loaiSinhVien',loaiSinhVien);
+console.log('diemToan',diemToan);
+console.log('diemVan',diemVan);
+// Hiển thị thông tin ng dùng lên giao diện
+document.querySelector('#spanTenSV').innerHTML = tenSinhVien
+document.querySelector('#spanMaSV').innerHTML = maSinhVien
+document.querySelector('#spanLoaiSV').innerHTML = loaiSinhVien
+// Gọi hàm tính điêm TB
+var diemTrungBinh = tinhDiemTrungBinh(diemToan,diemVan)
+document.querySelector('#spanDTB').innerHTML = diemTrungBinh;
+// Gọi hàm xếp loại SV
+var xeploai = xeploaiSinhVien(diemTrungBinh);
+document.querySelector('#spanXepLoai').innerHTML = xeploai
+}
+
+
+//Thông tin không có sẵn ===> PHẢI VIẾT HÀM FUNCTION()
+
+
+function tinhDiemTrungBinh(dToan,dVan){
+var diemTrungBinh = 0;//Output:number
+diemTrungBinh = (Number(dToan)+ Number(dVan))/2;
+return diemTrungBinh
+}
+
+function xeploaiSinhVien(dTrungBinh){//input
+    //output: "đậu ", "rớt"
+    var xeploai = ""
+    if(dTrungBinh >=5 ){
+        xeploai="Đậu"
+    }else{
+        xeploai="Rớt"
+    }
+return xeploai
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ QLSV/js/oop.js
@@ -0,0 +1,96 @@
+/**
+ Đối tượng trong javascript
+==> đưa biến và hàm về đúng vị trí của đói tượng đó
+
+ */
+
+
+// var product = {
+//     id:1,
+//     name :'ip9',
+//     price: 7675,
+//     showInfo: function(){
+//         console.log('id',product.id);
+//         console.log('name',product.name);
+//         console.log('price',product.price);
+
+//     }
+// }
+
+// var student = {
+//     id:2,
+//     name:'Nguyễn lê hưng' ,
+//      showInfo: function(){
+//          console.log('id',student.id);
+//          console.log('name',student.name);
+//     }
+// }
+
+
+// //Truy xuất đềnn các biến trong đối tượng(thuộc tính)
+// console.log('id',product.id);
+// console.log('id',product['id']);
+// console.log('tên sv',student.name);
+
+
+// // Truy xuất phương thức cho đối tượng
+// product.showInfo();
+// student['showInfo']()
+
+var sinhVien = {
+    maSInhVien:'',
+    tenSInhVien:'',
+    loaiSinhVien:'',
+    diemToan:0,
+    diemVan:0,
+    tinhDiemTrungBinh : function(){
+        // this chứa trong đối tượng thì this chính là đối tượng đó
+        //this.diemToan <==>sinhVien.diemToan
+         var diemTrungBinh = (Number(this.diemToan)+ Number(this.diemVan))/2;
+         return diemTrungBinh
+    },
+    xeploaiSinhVien: function(){
+        var dtb= this.tinhDiemTrungBinh();
+        var xeploai="";
+        if(dtb>=5){
+            xeploai="Đậu";
+        }else{
+            xeploai= "Rớt"
+        }
+        return xeploai
+    }
+}
+document.querySelector('#btnHienThi').onclick = function(){
+
+// Luu thong tin gn dùng nhập tử giao diện
+sinhVien.maSInhVien=document.querySelector('#txtMaSV').value;
+sinhVien.tenSInhVien=document.querySelector('#txtTenSV').value;
+sinhVien.loaiSInhVien=document.querySelector('#loaiSV').value;
+sinhVien.diemToan=document.querySelector('#txtDiemToan').value;
+sinhVien.diemVan=document.querySelector('#txtDiemVan').value;
+
+
+console.log('sinh viên:',sinhVien);
+//Dom đén các thê trên giao diện để hiển thị thông tin từ đói tượng
+// Hiển thị thông tin ng dùng nhập lên giao diện
+document.querySelector('#spanTenSV').innerHTML = sinhVien.tenSInhVien;
+document.querySelector('#spanMaSV').innerHTML = sinhVien.maSInhVien;
+document.querySelector('#spanLoaiSV').innerHTML = sinhVien.loaiSInhVien;
+document.querySelector('#spanDTB').innerHTML = sinhVien.tinhDiemTrungBinh();
+document.querySelector('#spanXepLoai').innerHTML = sinhVien.xeploaiSinhVien();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
